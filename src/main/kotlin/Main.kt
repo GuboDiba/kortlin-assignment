983a061b668fc46700d0608be11d68ae1cd9c267
Gubo Diba <gubodibawario@gmail.com>
2023-02-02 09:52:53 +0300

    created student record

---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,8 @@
+fun main(args: Array<String>) {
+    var name:String="Gubo Diba"
+    var age = 22
+    var contact: Long = +254798789069
+    var weight: Byte = 45
+    val kenyan: Boolean = true
+    println(kenyan)
+}
